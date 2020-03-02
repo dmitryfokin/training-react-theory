a4e27c6fa5d2a1b6ff5c784229ab256d4a9e19d2
Dmitry Fokin <dkoder.dvl@gmail.com>
2020-03-02 23:22:24 +0300

    less007

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>Hi!</h1>
-  //   </div>
-  // );
+  return (
+    <div>
+      <div className="App">
+        <h1>Hi!</h1>
+      </div>
+      <p>Hell</p>
+    </div>
+  );
 
-  return React.createElement(
-    'div',
-    {
-      className: 'App'
-    },
-    React.createElement('' +
-      'h1',
-      null,
-      '!Hi!')
-  )
+  // return React.createElement(
+  //   'div',
+  //   {
+  //     className: 'App'
+  //   },
+  //   React.createElement('' +
+  //     'h1',
+  //     null,
+  //     '!Hi!')
+  // )
 }
 
 export default App;
