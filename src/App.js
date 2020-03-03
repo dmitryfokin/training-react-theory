06359881e4976eb8343f67ec01cf4de97388715b
Dmitry Fokin <dkoder.dvl@gmail.com>
2020-03-03 14:08:14 +0300

    less016

--- src/App.js
+++ src/App.js
@@ -13,8 +13,9 @@ class App extends Component {
     pageTitle: 'React'
   }
 
-  changeTitleHandler = () => {
-    this.setState({pageTitle: '-- React --'})
+  changeTitleHandler = (newTitle, newTitleSufiks) => {
+    console.log(newTitleSufiks)
+    this.setState({pageTitle: newTitle + (typeof newTitleSufiks === 'String' ? newTitleSufiks : '')})
   }
 
   render() {
@@ -28,11 +29,23 @@ class App extends Component {
       <div className="App" style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
 
-        <button onClick={this.changeTitleHandler}>Change title</button>
+        <button onClick={this.changeTitleHandler.bind(this, ' -- React -- ')}>Change title</button>
 
-        <Car name={cars[0].name} year={cars[0].year}/>
-        <Car name={cars[1].name} year={cars[1].year}/>
-        <Car name={cars[2].name} year={cars[2].year}/>
+        <Car
+          name={cars[0].name}
+          year={cars[0].year}
+          onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name, cars[0].year.toString())}
+        />
+        <Car
+          name={cars[1].name}
+          year={cars[1].year}
+          onChangeTitle={() => this.changeTitleHandler(cars[1].name)}
+        />
+        <Car
+          name={cars[2].name}
+          year={cars[2].year}
+          onChangeTitle={() => this.changeTitleHandler(cars[2].name)}
+        />
       </div>
     );
   }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -4,5 +4,6 @@ export default props => (
   <div>
     <p>Name:<strong>{props.name}</strong></p>
     <p>Year: <strong>{props.year}</strong></p>
+    <button onClick={props.onChangeTitle}>clickni</button>
   </div>
 )
