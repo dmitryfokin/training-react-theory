0c7d1892242d8b926ddda738a984faba4d2cd5df
Dmitry Fokin <dkoder.dvl@gmail.com>
2020-03-03 09:08:03 +0300

    less015

--- src/App.js
+++ src/App.js
@@ -4,38 +4,38 @@ import './App.css';
 import Car from "./Car/Car";
 
 class App extends Component {
+  state = {
+    cars: [
+      {name: 'UAZ', year: 2015},
+      {name: 'Audi', year: 2016},
+      {name: 'Mazda', year: 2010},
+    ],
+    pageTitle: 'React'
+  }
+
+  changeTitleHandler = () => {
+    this.setState({pageTitle: '-- React --'})
+  }
+
   render() {
     const divStyle = {
       'textAlign': 'center'
     }
 
+    const cars = this.state.cars
+
     return (
       <div className="App" style={divStyle}>
-        <h1>Hi!</h1>
+        <h1>{this.state.pageTitle}</h1>
 
-        <Car name={'UAZ'} year={(2015)}>
-          <p style={{color: 'blue'}}>Color:</p>
-          <p style={{color: 'green'}}>Prise:</p>
-        </Car>
-        <Car name="Audi" year={2016}>
-          <p style={{color: 'red'}}>Color:</p>
-          <p style={{color: 'green'}}>Prise:</p>
-        </Car>
-        <Car name={'Mazda'} year={(2010)}/>
+        <button onClick={this.changeTitleHandler}>Change title</button>
+
+        <Car name={cars[0].name} year={cars[0].year}/>
+        <Car name={cars[1].name} year={cars[1].year}/>
+        <Car name={cars[2].name} year={cars[2].year}/>
       </div>
     );
   }
-
-  // return React.createElement(
-  //   'div',
-  //   {
-  //     className: 'App'
-  //   },
-  //   React.createElement('' +
-  //     'h1',
-  //     null,
-  //     '!Hi!')
-  // )
 }
 
 export default App;
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -2,9 +2,7 @@ import React from "react";
 
 export default props => (
   <div>
-    {console.log(props)}
     <p>Name:<strong>{props.name}</strong></p>
     <p>Year: <strong>{props.year}</strong></p>
-    {props.children}
   </div>
 )
